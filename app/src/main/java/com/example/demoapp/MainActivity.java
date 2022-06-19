e1f9475e206278714cb87bb4f786153bea4a9407
JaniHegedus <90450741+JaniHegedus@users.noreply.github.com>
2022-06-19 03:21:18 +0200

    commit

--- app/src/main/java/com/example/demoapp/MainActivity.java
+++ app/src/main/java/com/example/demoapp/MainActivity.java
@@ -3,9 +3,24 @@ package com.example.demoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void disable(View v)
+    {
+        v.setEnabled(false);
+    }
+    public void enable(View v)
+    {
+        v.setEnabled(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                v.setEnabled(true);
+            }
+        }, 1000);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
